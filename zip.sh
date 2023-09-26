26ed1ce17cba1aa10e86bcf72d92c10adb3a3799
DvirDukhan <dvir@redis.com>
2023-09-26 22:53:03 +0300

    macos nicks

--- zip.sh
+++ zip.sh
@@ -12,7 +12,8 @@ if [[ $os = 'Darwin' ]]
 then
     os='macos'
     OS_NICK=$(sw_vers -productVersion)
-    OS_NICK=${OS_NICK//./_}
+    # get major version
+    OS_NICK=${OS_NICK%%.*}
 else
     VERSION=$(grep '^VERSION_ID' /etc/os-release | sed 's/"//g')
     VERSION=${VERSION#"VERSION_ID="}
@@ -30,17 +31,26 @@ declare -A archs=(
     ["arm64v8"]="arm64v8"
 )
 
-declare -A OSNICKS=(
-    ["rocky8"]="rhel8"
-    ["centos7"]="rhel7"
-    ["ubuntu18.04"]="ubuntu18.04"
-    ["ubuntu20.04"]="ubuntu20.04"
-    ["ubuntu22.04"]="ubuntu22.04"
-    ["amazonlinux2"]="amzn2"
-)
-
 # Get OS nick name
-OS_NICK=${OSNICKS[$OS_NICK]}
+if [[ $os = 'macos' ]]
+then
+    declare -A MACOS_OSNICKS=(
+        ["13"]="ventura"
+        ["12"]="monterey"
+    )
+    OS_NICK=${MACOS_OSNICKS[$OS_NICK]}
+else
+    declare -A LINUX_OSNICKS=(
+        ["rocky_linux8.8"]="rhel8"
+        ["centos_linux7"]="rhel7"
+        ["ubuntu18.04"]="ubuntu18.04"
+        ["ubuntu20.04"]="ubuntu20.04"
+        ["ubuntu22.04"]="ubuntu22.04"
+        ["amazon_linux2"]="amzn2"
+        ["debian_gnu/linux11"]="bullseye"
+    )
+    OS_NICK=${LINUX_OSNICKS[$OS_NICK]}
+fi
 echo $OS_NICK
 
 # Get architecture
@@ -48,5 +58,6 @@ arch=${archs[$arch]}
 echo $arch
 
 # Call zip and create the zip file
+echo "Creating zip file ./bin/rediscompat-${os}-${OS_NICK}-${arch}.${version}.zip"
 zip -j ./bin/rediscompat-${os}-${OS_NICK}-${arch}.${version}.zip ./bin/rediscompat.so
 rm ./bin/rediscompat.so
